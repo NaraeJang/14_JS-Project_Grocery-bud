240cb95d69de37f030c28f276d99f088af4bc546
NaraeJang <j.naaaarae@gmail.com>
2023-05-25 00:23:27 -0400

    updated JS: add items

--- app.js
+++ app.js
@@ -19,10 +19,38 @@ form.addEventListener("submit", addItem);
 // ****** FUNCTIONS **********
 function addItem(e) {
   e.preventDefault();
+
   const value = grocery.value;
   const id = new Date().getTime().toString();
   if (value && !editFlag) {
-    console.log("add item to the list");
+    const element = document.createElement("article");
+    // add class
+    element.classList.add("grocery-item");
+    // add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-contianer">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+
+    // append child
+    list.appendChild(element);
+    // display alert
+    displayAlert("item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
+    // add to local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
   } else if (value && editFlag) {
     console.log("editing");
   } else {
@@ -41,6 +69,12 @@ function displayAlert(text, action) {
     alert.classList.remove(`alert-${action}`);
   }, 1500);
 }
+// set back to default
+function setBackToDefault() {
+  console.log("set back to default");
+}
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {
+  console.log("added to local storage");
+}
 // ****** SETUP ITEMS **********
