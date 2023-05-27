7f7678779d9203a10fef9630a0db5a120fb85318
NaraeJang <j.naaaarae@gmail.com>
2023-05-26 23:03:51 -0400

    updated JS:remove items from local storage

--- app.js
+++ app.js
@@ -138,17 +138,26 @@ function setBackToDefault() {
 function addToLocalStorage(id, value) {
   //   console.log("added to local storage");
   const grocery = { id, value };
-  let items = localStorage.getItem("list")
-    ? JSON.parse(localStorage.getItem("list"))
-    : [];
-
+  let items = getLocalStorage();
   items.push(grocery);
   localStorage.setItem("list", JSON.stringify(items));
-
-  console.log(items);
 }
-function removeFromLocalStorage(id) {}
+function removeFromLocalStorage(id) {
+  let items = getLocalStorage();
+
+  items = items.filter(function (item) {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
 function editLocalStorage(id, value) {}
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
 // ****** SETUP ITEMS **********
 // setItem
 // getItem
